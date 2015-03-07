9775a50f84b7b69eb30624c821915319705a8ce4
diegoelias <diego.uesc@gmail.com>
2015-03-07 13:49:59 -0300

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -22,6 +22,6 @@ with(data, {
 legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-## Generating Plot 3 (PNG)	   
+## Generating Plot 3 (PNG 480x480)	   
 dev.copy(png, file = "plot3.png", width = 480, height = 480)
 dev.off()
